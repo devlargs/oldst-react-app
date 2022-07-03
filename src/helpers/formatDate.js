213d3ce8d2421e66d99e625e37a9c079fb3e8f75
Ralph Largo <devlargs@gmail.com>
2022-07-03 15:42:41 +0800

    feat: render date properly based on the given condition

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -2,7 +2,16 @@ const fromNow = (dateString) => new Intl.RelativeTimeFormat().format(-5, "day");
 
 const formatDate = (dateString) => dateString.substr(4, 11);
 
+const subtractDate = (dateString) => {
+  const now = new Date();
+  const postedDate = new Date(dateString);
+  const diffTime = Math.abs(postedDate - now);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  return diffDays;
+};
+
 export const renderDate = (dateString) => {
-  const daysOld = +fromNow(dateString).split(" ")[0];
+  console.log(subtractDate(dateString));
+  const daysOld = subtractDate(dateString);
   return daysOld < 8 ? fromNow(dateString) : formatDate(dateString);
 };
